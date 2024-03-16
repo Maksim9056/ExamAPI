72592f86bb7e7a01d3a42a07a5c117c898f0f487
Maks <bobreczovm@inbox.ru>
2024-03-16 13:53:14 +0300

    Правки

--- ExamAPI/Program.cs
+++ ExamAPI/Program.cs
@@ -1,6 +1,9 @@
 
 using ExamAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace ExamAPI
 {
@@ -10,10 +13,38 @@ namespace ExamAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+       
 
             builder.Services.AddDbContext<ExamAPIContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("ExamAPIContext") ?? throw new InvalidOperationException("Connection string 'ExamAPIContext' not found.")));
+            // Add services to the container.
+            // Создание нового экземпляра сервиса DbContext и применение миграций
+            using (var serviceScope = builder.Services.BuildServiceProvider().CreateScope())
+            {
+                //var migrationDbContext = serviceScope.ServiceProvider.GetRequiredService<ExamAPIContext>();
+
+                //if (!migrationDbContext.Database.GetPendingMigrations().Any())
+                //{
+                //    var migrator = migrationDbContext.GetInfrastructure().GetService<IMigrator>();
+                //    migrator.Migrate();
+                //}
+                var serviceProvider = serviceScope.ServiceProvider;
+
+                // Получение экземпляра контекста базы данных
+                var migrationDbContext = serviceProvider.GetRequiredService<ExamAPIContext>();
+
+                // Создание миграции, если они отсутствуют
+                if (migrationDbContext.Database.GetPendingMigrations().Any())
+                {
+                    var migrator = migrationDbContext.GetService<IMigrator>();
+                    migrator.Migrate();
+                }
+
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ExamAPIContext>();
+                // Применение всех ожидающих миграций
+                dbContext.Database.Migrate();
+            }
+
 
             builder.Services.AddControllers();
 
